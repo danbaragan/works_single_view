8ab8c93fbb3ec79a89fa4ac2e8d2a011a6f9d253
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2019-03-11 23:00:47 +0200

    Better organize routes and views

--- web/wsv/__init__.py
+++ web/wsv/__init__.py
@@ -1,18 +1,15 @@
-import io
 import os
 from pathlib import Path
-from tempfile import TemporaryFile
 
-from flask import Flask, Response, send_file, request, render_template, flash, url_for, redirect
+from flask import Flask
 from flask_restful import Api
-from werkzeug.exceptions import BadRequestKeyError
-
-from . import db
-from . import importer
-from . import view
 
 
 def create_app(test_config=None):
+    from . import db
+    from . import importer
+    from . import view
+
     app = Flask(__name__)
 
     instance_path = Path(app.instance_path)
@@ -32,7 +29,10 @@ def create_app(test_config=None):
 
     if test_config:
         app.config.from_mapping(test_config)
-    api = Api(app)
+
+    app.register_blueprint(view.app_blueprint)
+
+    api = Api(app, '/api/v1')
     api.add_resource(view.WorksList, "/works")
     api.add_resource(view.WorksDetail, "/works/<string:iswc>")
 
@@ -40,37 +40,5 @@ def create_app(test_config=None):
     db.db_wrapper.database.close()
     db.init_app(app)
     importer.init_app(app)
-
-    @app.route('/hello/')
-    @app.route('/hello/<name>')
-    def hello(name='world'):
-        return render_template('hello.html', name=name)
-
-    @app.route('/import', methods=['GET', 'POST'])
-    def import_csv():
-        if request.method == 'POST':
-            try:
-                f = request.files['csv_file']
-                stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
-            except (BadRequestKeyError, UnicodeDecodeError):
-                return "no/bad file", 400
-
-            importer.csv_data.import_csv(stream)
-            flash(f'{f.filename} imported')
-            redirect(url_for('import_csv'))
-        return render_template('import.html')
-
-    @app.route('/export')
-    def export_csv():
-        # this will be deleted at close / garbage collection
-        file = TemporaryFile(mode="w+")
-        importer.csv_data.export_csv(file)
-        file.seek(0)
-        # return send_file(file, mimetype="text/csv")
-        return Response(
-            file,
-            mimetype="text/csv",
-            headers={"Content-disposition": "attachment; filename=works.csv"},
-        )
 
     return app
--- web/wsv/view.py
+++ web/wsv/view.py
@@ -1,7 +1,50 @@
+import io
+
+from flask import Blueprint
+from flask import Response, request, render_template, flash, url_for, redirect
 from flask_restful import Resource
+from werkzeug.exceptions import BadRequestKeyError
+from tempfile import TemporaryFile
 
 from .db import *
 from .serializer import *
+from . import importer
+
+app_blueprint = Blueprint('app', __name__)
+
+
+@app_blueprint.route('/hello/')
+@app_blueprint.route('/hello/<name>')
+def hello(name='world'):
+    return render_template('hello.html', name=name)
+
+
+@app_blueprint.route('/import', methods=['GET', 'POST'])
+def import_csv():
+    if request.method == 'POST':
+        try:
+            f = request.files['csv_file']
+            stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+        except (BadRequestKeyError, UnicodeDecodeError):
+            return "no/bad file", 400
+
+        importer.csv_data.import_csv(stream)
+        flash(f'{f.filename} imported')
+        redirect(url_for('import_csv'))
+    return render_template('import.html')
+
+
+@app_blueprint.route('/export')
+def export_csv():
+    # this will be deleted at close / garbage collection
+    file = TemporaryFile(mode="w+")
+    importer.csv_data.export_csv(file)
+    file.seek(0)
+    return Response(
+        file,
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=works.csv"},
+    )
 
 
 class WorksList(Resource):
