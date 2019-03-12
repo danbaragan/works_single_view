f0f35f1c06bb14165d140b60ce163f66f9e2aefa
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2019-03-12 13:54:42 +0200

    Fix tests to match new endpoints and return type

--- web/tests/test_factory.py
+++ web/tests/test_factory.py
@@ -2,14 +2,11 @@ from flask import current_app
 import pytest
 
 
-@pytest.mark.skip("This does very bad things to our session scoped app fixture")
-def test_config():
-    from wsv import create_app
-    assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
-
-
-def test_hello(client):
+@pytest.mark.parametrize('url_in,bytes_out', [
+    ('', b"Hello world!"),
+    ('dan', b"Hello dan!"),
+])
+def test_hello(client, url_in, bytes_out):
     assert current_app.testing
-    response = client.get('/hello')
-    assert response.data == b"Hello world!"
+    response = client.get(f'/hello/{url_in}')
+    assert bytes_out in response.data
